bca0aa04572f7afd737c855c9c5348d28c7f3ae2
Apostolos Apostolidis <apost.tolos@icloud.com>
2019-10-10 01:43:44 +0200

    add test for comparing coordinates

--- Tests/LocationManagerTests/LocationManagerTests.swift
+++ Tests/LocationManagerTests/LocationManagerTests.swift
@@ -2,10 +2,13 @@ import XCTest
 @testable import LocationManager
 
 final class LocationManagerTests: XCTestCase {
-    func testExample() {
+    func testCoordinatesEqual() {
+        let coordinates1 = Coordinates(withLatitude: 10.0, andLongitude: 11.0)
+        let coordinates2 = Coordinates(withLatitude: 10.0, andLongitude: 11.0)
+        assert(coordinates1 == coordinates2)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testCoordinatesEqual),
     ]
 }
